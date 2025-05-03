6dbc15209c541984a1bb00a8cc4217b9d8e5893e
kushbhardwaj0 <kush@kushbhardwaj.com>
2025-05-03 13:38:31 -0500

    implemented basic saving points and change tab watching

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,1 +1,9 @@
 console.log('background script loaded');
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  console.log("Tab changed to ID:", activeInfo.tabId);
+
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    console.log("Now active tab URL is:", tab.url);
+  });
+});
--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '@assets/img/logo.svg';
 import '@pages/newtab/Newtab.css';
 
 export default function Newtab() {
+  const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    const storedPoints = localStorage.getItem('points');
+    let currentPoints = storedPoints ? parseInt(storedPoints, 10) : 0;
+    currentPoints += 50;
+    localStorage.setItem('points', currentPoints.toString());
+    setPoints(currentPoints);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          You have <strong>{points}</strong> points.
+        </p>
         <p>
           Edit <code>src/pages/newtab/Newtab.tsx</code> and save to reload.
         </p>
